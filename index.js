57939e82a9048d59fd639226bf7a97de0a951ef1
yodafolio <af34532292@gmail.com>
2022-03-28 18:05:23 +0900

    세팅

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3100;
