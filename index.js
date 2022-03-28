b931e7342fdf5f909ec26f3a25c22bfa0b59fac7
pmg752290 <pmg752290@limefriends.com>
2022-03-28 14:03:52 +0900

    push test

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.use(cors(corsOption));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-     console.log('Cookies: ', req.cookies);
-     res.send('Hello Node.js');
+    console.log('Cookies: ', req.cookies);
+    res.send('Hello Node.js');
 });
 
 app.listen(part, () => {
-    console.log('Listening...(서버 실행중...)');
+    console.log('Listening..(서버 실행중...)');
 })
