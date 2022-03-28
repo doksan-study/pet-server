74427ed4fae21728610caa62b74f223dc3eb921d
yodafolio <af34532292@gmail.com>
2022-03-28 13:37:18 +0900

    프로젝트 세팅중

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const port = 3100;
+
+let corsOption = {
+    origin: ['http://localhost:8080'],
+    credential: true,
+}
+
+app.use(cors(corsOption));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+     console.log('Cookies: ', req.cookies);
+     res.send('Hello Node.js');
+});
+
+app.listen(part, () => {
+    console.log('Listening...(서버 실행중...)');
+})
