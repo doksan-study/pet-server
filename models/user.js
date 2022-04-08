034363288c7ed1b81bdb6f026a45a1b6b3ec6d06
pmg7522 <pmg752290@gmail.com>
2022-04-08 15:51:02 +0900

    Pet 모델 관계 설정

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       this.hasMany(models.Post);
       this.hasMany(models.Comment);
+      this.hasMany(models.Pet);
     }
   }
   User.init({
